53523e6a14727153bb36996a4085444705f702de
Adrián Serrano <aserranod@minsait.com>
2020-05-21 20:57:01 +0200

    Remove usePreviousWithCallback to include in usePrevious

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { default as usePrevious } from './usePrevious'
-export { default as usePreviousWithCallback } from './usePreviousWithCallback'
 export { default as useStateWithCallback } from './useStateWithCallback'
 export { default as useStateWithCallbackSync } from './useStateWithCallbackSync'
 export { default as useWatch } from './useWatch'
